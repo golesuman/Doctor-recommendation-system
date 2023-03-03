db5746081e7742f80031ea33a83dc0660059ca1c
Suman Gole <gole35830@gmail.com>
2023-03-03 19:45:32 +0545

    feat: create models

--- HospitalManagement/api/admin.py
+++ HospitalManagement/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Doctor, Hospital, Specialty
+
 # Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(Specialty)
+admin.site.register(Hospital)
--- HospitalManagement/api/models.py
+++ HospitalManagement/api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Specialty(models.Model):
+    name_of_specialty = models.CharField(max_length=255)
+    symptoms = models.TextField(max_length=400)
+
+    def __str__(self) -> str:
+        return str(self.name_of_specialty)
+
+    class Meta:
+        verbose_name = "specialty"
+        verbose_name_plural = "specialties"
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    specialty = models.ForeignKey(
+        Specialty,
+        on_delete=models.CASCADE,
+    )
+    phone_number = models.CharField(max_length=10)
+    email = models.CharField(max_length=255)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.name) + str(self.specialty)
+
+    class Meta:
+        verbose_name = "Doctor"
+        verbose_name_plural = "Doctors"
