b7608d6bd62d49748b33ad0ac5fcce023e8e186d
Suman Gole <gole35830@gmail.com>
2023-03-04 17:12:17 +0545

    chore: filtering and listing in admin panel

--- HospitalManagement/api/admin.py
+++ HospitalManagement/api/admin.py
@@ -2,8 +2,44 @@ from django.contrib import admin
 
 from .models import Doctor, Hospital, Specialty
 
-# Register your models here.
+# Register your models below
 
-admin.site.register(Doctor)
-admin.site.register(Specialty)
-admin.site.register(Hospital)
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "specialty",
+        "phone_number",
+        "email",
+        "hospital",
+    )
+    list_filter = (
+        "name",
+        "hospital",
+        "specialty",
+    )
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = (
+        "name_of_specialty",
+        "disease",
+    )
+    list_filter = (
+        "name_of_specialty",
+        "disease",
+    )
+
+
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "address",
+    )
+    list_filter = (
+        "name",
+        "address",
+    )
